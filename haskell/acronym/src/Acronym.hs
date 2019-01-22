e51b9296d7f8c000420cddc0771565c1886337c6
Matthew Reishus <mreishus@users.noreply.github.com>
2019-01-22 04:13:49 +0000

    Ran hfmt but collapsed my dash_to_space to one line

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,13 +1,15 @@
-module Acronym (abbreviate) where
+module Acronym
+  ( abbreviate
+  ) where
 
-import qualified Data.Char (toUpper, isUpper, isLower)
+import qualified Data.Char (isLower, isUpper, toUpper)
 
 abbreviate :: String -> String
 abbreviate xs = concat $ map abbrev_word $ Prelude.words $ dash_to_space xs
 
 -- Converts dashes to spaces.
 dash_to_space :: String -> String
-dash_to_space xs = map (\c -> if c=='-' then ' '; else c) xs
+dash_to_space xs = map (\c -> if c == '-' then ' ' else c) xs
 
 -- Given a word, abbreviates it.
 -- Usually picks the first letter and capitalizes it: "Cat" -> "C"
@@ -15,8 +17,9 @@ dash_to_space xs = map (\c -> if c=='-' then ' '; else c) xs
 abbrev_word :: String -> String
 abbrev_word w = go w (num_upper w) (num_lower w)
   where
-    go _ u l | u > 1 && l > 1 = filter Data.Char.isUpper w
-             | otherwise      = [Data.Char.toUpper(head w)]
+    go _ u l
+      | u > 1 && l > 1 = filter Data.Char.isUpper w
+      | otherwise = [Data.Char.toUpper (head w)]
 
 -- Number of uppercase chars in a string.
 num_upper :: String -> Int
@@ -25,8 +28,6 @@ num_upper w = length $ filter Data.Char.isUpper w
 -- Number of lowercase chars in a string.
 num_lower :: String -> Int
 num_lower w = length $ filter Data.Char.isLower w
-
-
 -- Bonus:
 -- Naive Algorithm that fails on "HyperText" -> "HT"
 --
