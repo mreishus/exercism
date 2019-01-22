e0d0abf169e3ed8c8f4531f9f29e7d5574f7a08b
Matthew Reishus <mreishus@users.noreply.github.com>
2019-01-22 02:39:46 +0000

    Ran hfmt on haskell/leap/src/LeapYear.hs

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,10 +1,13 @@
-module LeapYear (isLeapYear) where
+module LeapYear
+  ( isLeapYear
+  ) where
 
 isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy year divisor = year `rem` divisor == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = let is_4 = isDivisibleBy year 4
-                      is_100 = isDivisibleBy year 100
-                      is_400 = isDivisibleBy year 400
-                  in is_4 && (not is_100 || is_400)
+isLeapYear year =
+  let is_4 = isDivisibleBy year 4
+      is_100 = isDivisibleBy year 100
+      is_400 = isDivisibleBy year 400
+   in is_4 && (not is_100 || is_400)
