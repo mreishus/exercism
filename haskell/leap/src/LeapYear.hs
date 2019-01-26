5f156e1121786131d0d174c455ecea004528c736
Matthew Reishus <mreishus@users.noreply.github.com>
2019-01-26 18:09:51 +0000

    Rewrite Haskell/LeapYear after comments from mentor (guards)

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -6,8 +6,8 @@ isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy year divisor = year `rem` divisor == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year =
-  let is_4 = isDivisibleBy year 4
-      is_100 = isDivisibleBy year 100
-      is_400 = isDivisibleBy year 400
-   in is_4 && (not is_100 || is_400)
+isLeapYear year
+    | year `isDivisibleBy` 400 = True
+    | year `isDivisibleBy` 100 = False
+    | year `isDivisibleBy` 4 = True
+    | otherwise = False
