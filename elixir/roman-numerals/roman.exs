9d2c847678d6c6cfb8947f581295c594d5cc5c61
Matthew Reishus <mreishus@users.noreply.github.com>
2019-01-31 03:49:11 +0000

    elixir/roman-numerals: tail call and a few cases collapsed

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -3,26 +3,21 @@ defmodule Roman do
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number), do: romanize(number)
+  def numerals(number), do: romanize("", number)
 
-  def romanize(0), do: ""
-  def romanize(1), do: "I"
-  def romanize(2), do: "II"
-  def romanize(3), do: "III"
-  def romanize(4), do: "IV"
-  def romanize(5), do: "V"
-  def romanize(6), do: "VI"
-  def romanize(7), do: "VII"
-  def romanize(8), do: "VIII"
-  def romanize(9), do: "IX"
-  def romanize(n) when n >= 10 and n < 40, do: "X" <> romanize(n - 10)
-  def romanize(n) when n >= 40 and n < 50, do: "XL" <> romanize(n - 40)
-  def romanize(n) when n >= 50 and n < 90, do: "L" <> romanize(n - 50)
-  def romanize(n) when n >= 90 and n < 100, do: "XC" <> romanize(n - 90)
-  def romanize(n) when n >= 100 and n < 400, do: "C" <> romanize(n - 100)
-  def romanize(n) when n >= 400 and n < 500, do: "CD" <> romanize(n - 400)
-  def romanize(n) when n >= 500 and n < 900, do: "D" <> romanize(n - 500)
-  def romanize(n) when n >= 900 and n < 1000, do: "CM" <> romanize(n - 900)
-  def romanize(n) when n >= 1000, do: "M" <> romanize(n - 1000)
+  def romanize(acc, 0), do: acc <> ""
+  def romanize(acc, n) when n >= 1 and n < 4, do: romanize(acc <> "I", n - 1)
+  def romanize(acc, 4), do: acc <> "IV"
+  def romanize(acc, n) when n >= 5 and n < 9, do: romanize(acc <> "V", n - 5)
+  def romanize(acc, 9), do: acc <> "IX"
+  def romanize(acc, n) when n >= 10 and n < 40, do: romanize(acc <> "X", n - 10)
+  def romanize(acc, n) when n >= 40 and n < 50, do: romanize(acc <> "XL", n - 40)
+  def romanize(acc, n) when n >= 50 and n < 90, do: romanize(acc <> "L", n - 50)
+  def romanize(acc, n) when n >= 90 and n < 100, do: romanize(acc <> "XC", n - 90)
+  def romanize(acc, n) when n >= 100 and n < 400, do: romanize(acc <> "C", n - 100)
+  def romanize(acc, n) when n >= 400 and n < 500, do: romanize(acc <> "CD", n - 400)
+  def romanize(acc, n) when n >= 500 and n < 900, do: romanize(acc <> "D", n - 500)
+  def romanize(acc, n) when n >= 900 and n < 1000, do: romanize(acc <> "CM", n - 900)
+  def romanize(acc, n) when n >= 1000, do: romanize(acc <> "M", n - 1000)
 
 end
