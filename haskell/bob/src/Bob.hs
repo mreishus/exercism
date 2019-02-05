aa61ad9524f0303f742d8d9a1b0f0ad8d6e5350b
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-04 20:26:48 -0600

    Clean up haskell/bob after mentor feedback

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -7,42 +7,15 @@ module Bob
 import           Data.Text (Text)
 import qualified Data.Text as T
 
-data Statement
-  = Silent
-  | YellQuestion
-  | Yell
-  | Question
-  | Normal
-  deriving (Show, Eq)
-
 responseFor :: Text -> Text
-responseFor xs
-  | s == Silent = "Fine. Be that way!"
-  | s == YellQuestion = "Calm down, I know what I'm doing!"
-  | s == Yell = "Whoa, chill out!"
-  | s == Question = "Sure."
+responseFor xs'
+  | isSilent = "Fine. Be that way!"
+  | isYell && isQuestion = "Calm down, I know what I'm doing!"
+  | isYell = "Whoa, chill out!"
+  | isQuestion = "Sure."
   | otherwise = "Whatever."
   where
-    s = parseStatement xs
-
-parseStatement :: Text -> Statement
-parseStatement xs'
-  | isSilent xs = Silent
-  | isYell xs && isQuestion xs = YellQuestion
-  | isYell xs = Yell
-  | isQuestion xs = Question
-  | otherwise = Normal
-  where xs = T.strip xs'
-
-isSilent :: Text -> Bool
-isSilent xs = T.length xs == 0
-
--- Does input contain at least one uppercase letter and no lowercase letters?
-isYell :: Text -> Bool
-isYell xs = xsUpper == xs && xsUpper /= xsLower
-  where
-    xsUpper = T.toUpper xs
-    xsLower = T.toLower xs
-
-isQuestion :: Text -> Bool
-isQuestion xs = T.last xs == '?'
+    xs = T.strip xs'
+    isSilent = T.null xs
+    isQuestion = T.last xs == '?'
+    isYell = T.toUpper xs == xs && T.toUpper xs /= T.toLower xs
