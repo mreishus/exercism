8e044026e50f4fe4eb8309d322425339bab64763
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-05 02:12:43 +0000

    update romanize

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -3,21 +3,21 @@ defmodule Roman do
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number), do: romanize("", number)
+  def numerals(number), do: roman("", number)
 
-  defp romanize(acc, n) when n >= 1000, do: romanize(acc <> "M", n - 1000)
-  defp romanize(acc, n) when n >= 900, do: romanize(acc <> "CM", n - 900)
-  defp romanize(acc, n) when n >= 500, do: romanize(acc <> "D", n - 500)
-  defp romanize(acc, n) when n >= 400, do: romanize(acc <> "CD", n - 400)
-  defp romanize(acc, n) when n >= 100, do: romanize(acc <> "C", n - 100)
-  defp romanize(acc, n) when n >= 90, do: romanize(acc <> "XC", n - 90)
-  defp romanize(acc, n) when n >= 50, do: romanize(acc <> "L", n - 50)
-  defp romanize(acc, n) when n >= 40, do: romanize(acc <> "XL", n - 40)
-  defp romanize(acc, n) when n >= 10, do: romanize(acc <> "X", n - 10)
-  defp romanize(acc, 9), do: acc <> "IX"
-  defp romanize(acc, n) when n >= 5, do: romanize(acc <> "V", n - 5)
-  defp romanize(acc, 4), do: acc <> "IV"
-  defp romanize(acc, n) when n >= 1, do: romanize(acc <> "I", n - 1)
-  defp romanize(acc, 0), do: acc
+  defp roman(acc, n) when n >= 1000, do: roman(acc <> "M", n - 1000)
+  defp roman(acc, n) when n >= 900, do: roman(acc <> "CM", n - 900)
+  defp roman(acc, n) when n >= 500, do: roman(acc <> "D", n - 500)
+  defp roman(acc, n) when n >= 400, do: roman(acc <> "CD", n - 400)
+  defp roman(acc, n) when n >= 100, do: roman(acc <> "C", n - 100)
+  defp roman(acc, n) when n >= 90, do: roman(acc <> "XC", n - 90)
+  defp roman(acc, n) when n >= 50, do: roman(acc <> "L", n - 50)
+  defp roman(acc, n) when n >= 40, do: roman(acc <> "XL", n - 40)
+  defp roman(acc, n) when n >= 10, do: roman(acc <> "X", n - 10)
+  defp roman(acc, 9), do: acc <> "IX"
+  defp roman(acc, n) when n >= 5, do: roman(acc <> "V", n - 5)
+  defp roman(acc, 4), do: acc <> "IV"
+  defp roman(acc, n) when n >= 1, do: roman(acc <> "I", n - 1)
+  defp roman(acc, 0), do: acc
 
 end
