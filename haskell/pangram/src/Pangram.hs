fa3a8b0a2a058173ced251037962bdd732fbff3a
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-05 18:15:05 -0600

    Haskell pangram: Switch from string to text

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,20 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Pangram (isPangram) where
 
 import qualified Data.Map.Strict as M
 import qualified Data.Char as C
 
-isPangram :: String -> Bool
-isPangram text = (M.size $ charCount filteredText ) >= 26
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+isPangram :: Text -> Bool
+isPangram text = (M.size $ charCount filteredText ) == 26
     where
-        filteredText = map C.toLower $ filter C.isLetter $ text
+        filteredText = T.map C.toLower $ T.filter C.isLetter $ text
 
 -- Constant: An empty map
 emptyMap :: Num a => M.Map Char a
 emptyMap = M.fromList []
 
 -- Given a string, convert it to a map of character frequency counts
-charCount :: Num a => String -> M.Map Char a
-charCount xs = foldl addCharToMap emptyMap xs
+charCount :: Num a => Text -> M.Map Char a
+charCount xs = T.foldl addCharToMap emptyMap xs
 
 -- Build up a map of character frequency counts by adding one character seen.
 addCharToMap :: Num a => M.Map Char a -> Char -> M.Map Char a
