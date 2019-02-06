92aa68b718a58a50a0fcd7be1643a9a55f423fba
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-05 18:03:18 -0600

    Haskell/pangram: Small cleanup

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,30 +1,21 @@
 module Pangram (isPangram) where
 
-import qualified Data.Map.Strict as Map
-import qualified Data.Char
+import qualified Data.Map.Strict as M
+import qualified Data.Char as C
 
 isPangram :: String -> Bool
-isPangram text = (Map.size $ charCount filteredText ) >= 26
+isPangram text = (M.size $ charCount filteredText ) >= 26
     where
-        filteredText = lowerCase $ filterLetter text
+        filteredText = map C.toLower $ filter C.isLetter $ text
 
 -- Constant: An empty map
-emptyMap :: Num a => Map.Map Char a
-emptyMap = Map.fromList []
+emptyMap :: Num a => M.Map Char a
+emptyMap = M.fromList []
 
 -- Given a string, convert it to a map of character frequency counts
-charCount :: Num a => String -> Map.Map Char a
+charCount :: Num a => String -> M.Map Char a
 charCount xs = foldl addCharToMap emptyMap xs
 
 -- Build up a map of character frequency counts by adding one character seen.
-addCharToMap :: Num a => Map.Map Char a -> Char -> Map.Map Char a
-addCharToMap aMap x = Map.insertWith (+) x 1 aMap
-
-
--- Return a lowercase version of a String
-lowerCase :: String -> String
-lowerCase xs = map Data.Char.toLower xs
-
-filterLetter :: String -> String
-filterLetter xs = filter Data.Char.isLetter xs
-
+addCharToMap :: Num a => M.Map Char a -> Char -> M.Map Char a
+addCharToMap aMap x = M.insertWith (+) x 1 aMap
