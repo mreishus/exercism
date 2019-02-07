55653b812e4fbaabca00e5149cdc292a796684cd
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-06 18:20:17 -0600

    Convert haskell/pangram to use set instead of map

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -5,24 +5,13 @@ module Pangram
   ) where
 
 import qualified Data.Char       as C
-import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
 import           Data.Text       (Text)
 import qualified Data.Text       as T
 
 isPangram :: Text -> Bool
-isPangram text = (M.size $ charCount filteredText) == 26
+isPangram text = (S.size $ charSet filteredText) == 26
   where
     filteredText = T.map C.toLower $ T.filter C.isLetter $ text
-
--- Constant: An empty map
-emptyMap :: Num a => M.Map Char a
-emptyMap = M.fromList []
-
--- Given a string, convert it to a map of character frequency counts
-charCount :: Num a => Text -> M.Map Char a
-charCount xs = T.foldl addCharToMap emptyMap xs
-
--- Build up a map of character frequency counts by adding one character seen.
-addCharToMap :: Num a => M.Map Char a -> Char -> M.Map Char a
-addCharToMap aMap x = M.insertWith (+) x 1 aMap
+    charSet xs = T.foldl (flip S.insert) S.empty xs
