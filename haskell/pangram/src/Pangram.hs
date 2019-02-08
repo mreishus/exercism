3d9d3760cb0a35cbb7ef5d1cfc542952a4e064b4
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-07 21:37:16 -0600

    haskell/pangram exits early if possible

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -7,11 +7,8 @@ module Pangram
 import qualified Data.Char as C
 import qualified Data.Set  as S
 
-import           Data.Text (Text)
-import qualified Data.Text as T
-
-isPangram :: Text -> Bool
-isPangram text = (S.size $ charSet filteredText) == 26
+isPangram :: String -> Bool
+isPangram text = elem 26 $ map S.size charSet
   where
-    filteredText = T.map C.toLower $ T.filter C.isLetter $ text
-    charSet xs = T.foldl (flip S.insert) S.empty xs
+    filteredText = map C.toLower $ filter C.isLetter $ text
+    charSet = scanl (flip S.insert) S.empty filteredText
