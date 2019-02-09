120a770fd973d440aa91d234d0182aa5ed2ab38f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-09 17:44:50 -0600

    Revision of haskell/collatz - experimental

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,6 +2,30 @@ module CollatzConjecture
   ( collatz
   ) where
 
+import Data.List (unfoldr, genericLength)
+
+-- dupe: Turns a single value into a tuple of that value repeated.
+dupe :: a -> (a,a)
+dupe x = (x,x)
+
+-- nextCollatz:  Finds the next number in the collatz sequence, or Nothing if done.
+-- Number is returned as a duplicate tuple so this can be used with unfoldr.
+nextCollatz :: Integer -> Maybe (Integer, Integer)
+nextCollatz n
+  | n <= 1 = Nothing
+  | n `mod` 2 == 0 = Just $ dupe $ n `div` 2
+  | otherwise = Just $ dupe $ n * 3 + 1
+
+-- collatz: Find the length of the collatz sequence beginning with n.
+collatz :: Integer -> Maybe Integer
+collatz n
+  | n < 1 = Nothing
+  | otherwise = Just $ genericLength $ unfoldr nextCollatz n
+
+{-|
+
+Recursive version w/ "go" function
+
 collatz :: Integer -> Maybe Integer
 collatz x = go x 0
   where
@@ -10,3 +34,4 @@ collatz x = go x 0
       | n == 1 = Just steps
       | n `mod` 2 == 0 = go (n `div` 2) (steps + 1)
       | otherwise = go (n * 3 + 1) (steps + 1)
+-}
