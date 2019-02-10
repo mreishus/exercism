5046f33346a9fb59f836249ed5254b1772fe0c82
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-10 15:05:06 -0600

    Improve haskell/dna again

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -3,13 +3,12 @@ module DNA
   ) where
 
 toRNA :: String -> Either Char String
-toRNA xs = concat <$> traverse toRNA1 xs
-
-toRNA1 :: Char -> Either Char String
-toRNA1 x =
-  case x of
-    'C' -> Right "G"
-    'G' -> Right "C"
-    'T' -> Right "A"
-    'A' -> Right "U"
-    _   -> Left x
+toRNA = mapM toRNA1
+  where
+    toRNA1 x =
+      case x of
+        'C' -> Right 'G'
+        'G' -> Right 'C'
+        'T' -> Right 'A'
+        'A' -> Right 'U'
+        _   -> Left x
