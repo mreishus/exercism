10a7ba49b14c26a2000c5097aff97c8ff6ea892d
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-17 17:02:44 -0600

    Add elixir/robot-simulator

---
+++ elixir/robot-simulator/robot_simulator.exs
@@ -0,0 +1,112 @@
+defmodule RobotSimulator do
+  @directions [:north, :east, :south, :west]
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ :north, position \\ {0, 0}) do
+    {dv, pv} = {
+      direction_valid?(direction),
+      position_valid?(position)
+    }
+
+    case {dv, pv} do
+      {true, true} ->
+        %{position: position, direction: direction}
+
+      {false, true} ->
+        {:error, "invalid direction"}
+
+      {true, false} ->
+        {:error, "invalid position"}
+
+      {false, false} ->
+        {:error, "invalid direction and position"}
+    end
+  end
+
+  # Is direction valid?
+  # (Atom) -> Bool
+  defp direction_valid?(direction), do: Enum.member?(@directions, direction)
+
+  # Is position valid?
+  # ({Integer, Integer}) -> Bool
+  defp position_valid?({x, y}) when is_integer(x) and is_integer(y), do: true
+  defp position_valid?(_), do: false
+
+  @doc """
+  Simulate the robot's movement given a string of instructions.
+
+  Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
+  """
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  def simulate(robot, instructions) do
+    instructions
+    |> String.graphemes()
+    |> Enum.reduce(robot, fn x, acc ->
+      case acc do
+        {:error, _} -> acc
+        _ -> process_inst(acc, x)
+      end
+    end)
+  end
+
+  # process_inst: Updates a robot after one instruction.
+  # "R", "L", and "A" are supported.
+  # (Robot, String) -> Robot
+  defp process_inst(robot, "R"), do: process_turn(robot, 1)
+  defp process_inst(robot, "L"), do: process_turn(robot, -1)
+
+  defp process_inst(%{direction: direction, position: position} = robot, "A") do
+    %{robot | position: advance_position(position, direction)}
+  end
+
+  defp process_inst(_robot, _), do: {:error, "invalid instruction"}
+
+  # process_turn: Updates a robot with a turn specified in number of 90 deg clockwise turns.
+  # (Robot, Integer) -> Robot
+  defp process_turn(%{direction: direction} = robot, direction_delta) do
+    new_dir =
+      @directions
+      |> Enum.find_index(fn x -> x == direction end)
+      |> Kernel.+(direction_delta)
+      |> rem(4)
+      |> (fn x -> Enum.at(@directions, x) end).()
+
+    %{robot | direction: new_dir}
+  end
+
+  # advance_position: Gives position after taking a step forward.
+  # (Position, Direction) -> Position
+  defp advance_position(position, direction) do
+    direction
+    |> direction_vector
+    |> addt(position)
+  end
+
+  # addt: Adds two tuples.
+  defp addt({a, b}, {c, d}), do: {a + c, b + d}
+
+  # direction_vector: Converts a direction (atom) into a unit vector.
+  # (Direction) -> Position
+  defp direction_vector(:north), do: {0, 1}
+  defp direction_vector(:south), do: {0, -1}
+  defp direction_vector(:west), do: {-1, 0}
+  defp direction_vector(:east), do: {1, 0}
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction(%{direction: d}), do: d
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(%{position: p}), do: p
+end
