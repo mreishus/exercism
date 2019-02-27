9f748e6e16a72b6fb26f0a9ee06ed312e39100f5
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-27 17:15:53 -0600

    Haskell/grains

---
+++ haskell/grains/src/Grains.hs
@@ -0,0 +1,17 @@
+module Grains
+  ( square
+  , total
+  ) where
+
+import           Data.Maybe (fromMaybe)
+
+square :: Integer -> Maybe Integer
+square n
+  | n <= 0 = Nothing
+  | n > 64 = Nothing
+  | otherwise = Just (2 ^ (n - 1))
+
+total :: Integer
+total = fromMaybe 0 $ sum <$> sequence squares
+  where
+    squares = map square [1 .. 64]
