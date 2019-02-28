0474ab66eb4b0c1a6a4c7c0857668521a384fb54
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-28 11:36:46 -0600

    Refactor haskell/grains to use catMaybes

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -3,15 +3,12 @@ module Grains
   , total
   ) where
 
-import           Data.Maybe (fromMaybe)
+import           Data.Maybe (catMaybes)
 
 square :: Integer -> Maybe Integer
 square n
-  | n <= 0 = Nothing
-  | n > 64 = Nothing
+  | n <= 0 || n > 64 = Nothing
   | otherwise = Just (2 ^ (n - 1))
 
 total :: Integer
-total = fromMaybe 0 $ sum <$> sequence squares
-  where
-    squares = map square [1 .. 64]
+total = sum . catMaybes $ map square [1 .. 64]
