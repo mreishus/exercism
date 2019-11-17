8e47a08add46db53b6881adb2b8a6234dff9c16d
Matthew Reishus <mreishus@users.noreply.github.com>
2019-11-16 23:57:21 -0600

    Update crystal/hamming

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,18 +1,11 @@
-# Please implement your solution to hamming in this file
 module Hamming
   def self.distance(s1 : String, s2 : String)
     if s1.size != s2.size
       raise ArgumentError.new()
     end
-    distance = 0
-    s1.chars.each_with_index do |c1, i|
-      c2 = s2.chars[i]
-      if c1 != c2
-        distance += 1
-      end
-    end
-    distance
+
+    0.upto(s1.size - 1)
+      .select { |i| s1.chars[i] != s2.chars[i] }
+      .size
   end
 end
-
-puts Hamming.distance("xxxx", "yyyy")
